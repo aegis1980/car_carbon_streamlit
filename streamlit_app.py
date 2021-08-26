c5bea72542a656b9a8b2293dd7e488cba313102d
aegis1980 <jonrobinson1980@gmail.com>
2021-08-26 18:47:13 +1200

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,169 @@
 from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
 import streamlit as st
+import plotly.graph_objects as go
 
 """
-# Welcome to Streamlit!
+# CO2: Car steel production vs in-service
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+Exploring Volvo's claim that 35% of a ICE car's CO2 production comes from steel production at manufacturing. 
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+I have gone with UK-based values with sources in the little question mark tooltips. 
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+Edit `/app.py` to customize this app to your heart's desire :heart: or play with the sliders below. 
 """
+
+MPG_US = 'mpg (US)'
+MPG_UK = 'mpg (UK)'
+L_100KM = 'L/100km'
+
+in_service = {
+    MPG_US : {
+        'label' : 'Fuel efficiency, MPG (US gallons)',
+        'unit_d' : 'miles',
+        'unit_v' : 'gallon (US)',
+        'efficiency': {
+            'def' : int(36 /1.2), 
+            'max' : 100,
+            'source' : 'https://www.nimblefins.co.uk/cheap-car-insurance/average-mpg'
+        },
+        'co2_per_vol' : {
+            'def' : 8.89,#kg/gal
+            'max' : 20.0,
+            'source' : 'https://www.eia.gov/environment/emissions/co2_vol_mass.php'
+        },
+        'longevity': {
+            'def' : 100000,
+            'max' : 300000
+        }
+    },
+    MPG_UK : {
+        'label' : 'Fuel efficiency, MPG (UK gallons)',
+        'unit_d' : 'miles',
+        'unit_v' : 'gallon (UK)',
+        'efficiency': {
+            'def' : 36, 
+            'max' : 100,
+            'source' : 'https://www.nimblefins.co.uk/cheap-car-insurance/average-mpg'
+        },
+        'co2_per_vol' : {
+            'def' : 8.89 * 1.2, # convservsion us gal to uk
+            'max' : 20.0,
+            'source' : 'https://www.eia.gov/environment/emissions/co2_vol_mass.php'
+        },
+        'longevity': {
+            'def' : 100000,
+            'max' : 300000
+        }
+    },
+    L_100KM : {
+        'label' : 'Fuel efficiency, L/100km',
+        'unit_d' : 'km',
+        'unit_v' : 'litre',
+        'efficiency': {
+            'def' : int(282.481/36),
+            'max' : 30,
+            'source' : 'https://www.nimblefins.co.uk/cheap-car-insurance/average-mpg'
+        },
+        'co2_per_vol' : {
+            'def' : 8.89 * 0.26, # us gal to litres
+            'max' : 20.0,
+            'source' : 'https://www.eia.gov/environment/emissions/co2_vol_mass.php'
+        },
+        'longevity': {
+            'def' : 200000,
+            'max' : 500000
+        }
+        
+    }
+}
 
 
 with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    st.header("Production")
+    mass_steel_car = st.slider("Mass of stell in car (kg)",
+        0,
+        3000,
+        900,
+        help='https://www.worldsteel.org/steel-by-topic/steel-markets/automotive.html'
+    )
 
-    points_per_turn = total_points / num_turns
+    percent_steel = st.slider(
+        "Percentage steel, by weight (%)",
+        0,100,
+        65,
+        help = 'https://www.worldautosteel.org/life-cycle-thinking/recycling/'
+    )
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    percent_recycled = st.slider(
+        "% recycled steel",
+        0,
+        100,
+        25,
+        help = 'https://www.worldautosteel.org/life-cycle-thinking/recycling/'
+    )
+    
+    co2_steel = st.slider(
+        "Tonnes of CO2 per tonne of steel (blast furnace)", 
+        0.0, 
+        5.0, 
+        1.85,
+        help = 'https://www.mckinsey.com/industries/metals-and-mining/our-insights/decarbonization-challenge-for-steel'
+    )
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    st.header("In-Service")
+    st.text("This is the CO2 produced by the vehicle over lifetime, prior to being scrapped")
+    st.text("Only includes the CO2 produced from burning fuel in a petrol internal combustion engine (ICE)")
+
+    units = st.radio(
+        "Units",
+        (MPG_UK,L_100KM,MPG_US )
+    )
+
+
+
+    fuel_usage = st.slider(in_service[units]['label'], 
+        0, 
+        in_service[units]['efficiency']['max'],
+        in_service[units]['efficiency']['def'],
+        help = in_service[units]['efficiency']['source']
+    )
+    co2_per_vol = st.slider(
+        f"kg CO2 per {in_service[units]['unit_v']} of fuel", 
+        value = in_service[units]['co2_per_vol']['def'],
+        max_value = in_service[units]['co2_per_vol']['max'],
+        help = in_service[units]['co2_per_vol']['source']
+    )
+
+    distance = st.slider(
+        f"Car longevity ({in_service[units]['unit_d']})",
+        0,
+        in_service[units]['longevity']['max'],
+        in_service[units]['longevity']['def']
+    )
+
+    if units == MPG_UK or units == MPG_US:
+        M_service = ((distance / fuel_usage) * co2_per_vol)/1000 
+    else: 
+        M_service = distance *(fuel_usage/100) * co2_per_vol/1000
+
+
+
+    M_production = (mass_steel_car * (1-percent_recycled/100) * co2_steel)/1000 # mass (tonnes) CO2 from production
+
+    colors = ['gold', 'mediumturquoise']
+
+    fig = go.Figure(data=[go.Pie(labels=['Production','In-service'],
+                                values=[M_production,M_service])])
+    fig.update_traces(hoverinfo='label+percent', textinfo='percent', textfont_size=20,
+                    marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+
+
+    st.plotly_chart(
+        fig
+    )
+
